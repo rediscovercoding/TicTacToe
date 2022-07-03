e4611a8522ec298416c0b87e3f1bf07e8085f182
unknown <koustubhdube@outlook.com>
2022-07-03 21:28:06 +0200

    Add final files

--- javascript.js
+++ javascript.js
@@ -1,27 +1,21 @@
 let turnNo = 0;
 
-//constructor function? is it necessary?
-function Player(name){
-    this.name = name;
-}
-//below function makes use of constructor and spits out the name of the player? 
 
-function bookPlayer(){
-    let Player1 = new Player(document.querySelector('#player1').value);
-    let Player2 = new Player(document.querySelector('#player2').value);
-    return{Player1, Player2}
-}
+
+
 //empty array to save the game board
-let gameboard=['a','b','c','d','e','f','g','h','i'];
+let gameboard=['','','','','','','','',''];
 
-
-
+ 
+//below function to display the marks and send it to gameboard array, then check for winning logic and send for announcement
 function display(n){
     let box= document.querySelector(`.box${n}`);
     let div= document.createElement('div');  
     let cellid = document.querySelector(`#cellid${n}`);
+    let WinnerClass = document.querySelector('.WinnerClass');
+    
     //display of spot or mark
-    if (turnNo < 9 && cellid === null){  //cellid ===null condition so that each mark is in unique box
+    if (turnNo < 9 && cellid === null && WinnerClass == null){  //cellid ===null condition so that each mark is in unique box
         if (turnNo%2 ==0){
             turnNo++;
             div.textContent='X';
@@ -37,8 +31,8 @@ function display(n){
     box.appendChild(div);
     }    
     //logic of winning after move no 5
-    if (turnNo > 0 && turnNo <9){
-        console.log(gameboard);
+    if (turnNo > 4 && turnNo <9 && WinnerClass == null){
+        
         let mark = gameboard[0].move;
         let mark2 = gameboard[5].move;
         let mark3 = gameboard[7].move;
@@ -48,7 +42,6 @@ function display(n){
             Winner(gameboard[4].move);
         }
         if(gameboard[0].move == mark && gameboard[1].move == mark && gameboard[2].move == mark){               
-            console.log('if2')
             Winner(gameboard[1].move);
         }   
         if(gameboard[0].move == mark && gameboard[3].move == mark && gameboard[6].move == mark){               
@@ -73,14 +66,14 @@ function display(n){
     if (turnNo == 9){
         Winner('Tie');
     }   
-    
+    return{box, n};
 }
 
 //below function to move the moves from display function into the empty array
 function gameplay(n, mark){
     let boxNo = `box${n}`;
     let moveObject = {cell:boxNo, move:mark};
-    gameboard.splice(n-1, 1, moveObject);
+    gameboard.splice(n-1, 1, moveObject);  
 }
 
 //function for winner announcement
@@ -88,16 +81,36 @@ function Winner(mark){
     let announce= document.querySelector('.announce');
     let p = document.createElement('p');
     if (mark == 'X'){
-        console.log('Winner is X');
-        p.textContent= 'X is the Winner';  
+        p.textContent= 'X is the Winner';
+        p.classList.add('WinnerClass');
     }
     else if (mark == 'O'){
-        console.log('Winner is  O');
         p.textContent= 'O is the Winner';
+        p.classList.add('WinnerClass');
     }
     else if (mark =='Tie'){
-        console.log('It is a Tie');
         p.textContent= 'It is a Tie';
+        p.classList.add('WinnerClass');
     }
+    p.setAttribute('id', 'Announcement');
     announce.appendChild(p);
+    
+}
+
+function reset(){
+    let announce= document.querySelector('.announce');
+    while (announce.lastElementChild) {
+        announce.removeChild(announce.lastElementChild);
+      }
+
+    for (let i = 0; i<10; i++){
+        let div= document.querySelector(`.box${i}`);
+        let child= document.querySelector(`#cellid${i}`);
+        if(child != null){
+            div.removeChild(child);
+        }
+    }
+    gameboard=['','','','','','','','',''];
+    turnNo = 0;
+    
 }
